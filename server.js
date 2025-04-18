d6ef222f02dca40865af5540473464d154292225
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2025-04-17 21:06:10 -0700

    removed unnecessary lines

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
 const cors = require("cors");
 const itemRoutes = require("./routes/itemRoutes"); // Import item routes
 const userRoutes = require("./routes/userRoutes"); // Import user routes
 
-const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
